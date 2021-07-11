42792148af51be02dbb138058c2d6da1907a8b8e
Serge Labada <sergelabada@gmail.com>
2021-07-11 14:17:42 +0300

    Задание 1 на оценку исправлено. Спасибо

--- src/exam/Exam01.java
+++ src/exam/Exam01.java
@@ -22,20 +22,17 @@ public class Exam01 {
         week = seconds / 3600 / 24 / 7;
 
         // склоняем секунды
-        if (sec >= 11 && sec <= 20) getSeconds = "секунд";
-        else if ((sec % 10 >= 5 && sec % 10 <= 9) ||  sec % 10 == 0) getSeconds = "секунд";
+        if ((sec % 10 >= 5 && sec % 10 <= 9) ||  sec % 10 == 0 || sec >= 11 && sec <= 20) getSeconds = "секунд";
         else if (sec % 10 >= 2 && sec % 10 <= 4) getSeconds = "секунды";
         else if (sec % 10 == 1) getSeconds = "секунда";
 
         // склоняем минуты
-        if (min >= 11 && min <= 20) getMinutes = "минут";
-        else if ((min % 10 >= 5 && min % 10 <= 9) ||  min % 10 == 0) getMinutes = "минут";
+        if ((min % 10 >= 5 && min % 10 <= 9) ||  min % 10 == 0 || min >= 11 && min <= 20) getMinutes = "минут";
         else if (min % 10 >= 2 && min % 10 <= 4) getMinutes = "минуты";
         else if (min % 10 == 1) getMinutes = "минута";
 
         // склоняем часы
-        if (hour >= 11 && hour <= 20) getHours = "часов";
-        else if ((hour % 10 >= 5 && hour % 10 <= 9) ||  hour % 10 == 0) getHours = "часов";
+        if ((hour % 10 >= 5 && hour % 10 <= 9) ||  hour % 10 == 0 || hour >= 11 && hour <= 20) getHours = "часов";
         else if (hour % 10 >= 2 && hour % 10 <= 4) getHours = "часа";
         else if (hour % 10 == 1) getHours = "час";
 
@@ -45,8 +42,7 @@ public class Exam01 {
         else if (day % 10 == 1) getDays = "день";
 
         // склоняем недели
-        if (week % 100 >= 11 && week % 100 <= 20) getWeeks = "недель";
-        else if ((week % 10 >= 5 && week % 10 <= 9) ||  week % 10 == 0) getWeeks = "недель";
+        if ((week % 10 >= 5 && week % 10 <= 9) ||  week % 10 == 0 || week % 100 >= 11 && week % 100 <= 20) getWeeks = "недель";
         else if (week % 10 >= 2 && week % 10 <= 4) getWeeks = "недели";
         else if (week % 10 == 1) getWeeks = "неделя";
 
