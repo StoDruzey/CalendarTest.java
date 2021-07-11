48598d3f706e5f204713c2e4cd6797df6034780f
Serge Labada <sergelabada@gmail.com>
2021-07-11 19:19:41 +0300

    Задание 1 на оценку еще раз (внимательнее) исправлено. Спасибо

--- src/exam/Exam01.java
+++ src/exam/Exam01.java
@@ -7,13 +7,13 @@ public class Exam01 {
         // Задание 1. Напишите программу, которая будет переводить секунды в форматированный вид - часы минуты секунды
         // Добавьте 3 строчку, которая будет выводить недели, сутки, часы минуты и секунды
 
-        int seconds = 3186399;
+        int seconds = 1114414;
         int hour, min, sec, day, week;
-        String getSeconds = new String();
-        String getMinutes = new String();
-        String getHours = new String();
-        String getDays = new String();
-        String getWeeks = new String();
+        String getSeconds = "секунд";
+        String getMinutes = "минут";
+        String getHours = "часов";
+        String getDays = "дней";
+        String getWeeks = "недель";
 
         sec = seconds % 60;
         min = seconds / 60 % 60;
@@ -22,29 +22,24 @@ public class Exam01 {
         week = seconds / 3600 / 24 / 7;
 
         // склоняем секунды
-        if ((sec % 10 >= 5 && sec % 10 <= 9) ||  sec % 10 == 0 || sec >= 11 && sec <= 20) getSeconds = "секунд";
-        else if (sec % 10 >= 2 && sec % 10 <= 4) getSeconds = "секунды";
-        else if (sec % 10 == 1) getSeconds = "секунда";
+        if ((sec % 10 >= 2 && sec % 10 <= 4) && !(sec % 100 >= 12 && sec % 100 <= 14)) getSeconds = "секунды";
+        else if (sec % 10 == 1 && sec % 100 != 11) getSeconds = "секунда";
 
         // склоняем минуты
-        if ((min % 10 >= 5 && min % 10 <= 9) ||  min % 10 == 0 || min >= 11 && min <= 20) getMinutes = "минут";
-        else if (min % 10 >= 2 && min % 10 <= 4) getMinutes = "минуты";
-        else if (min % 10 == 1) getMinutes = "минута";
+        if ((min % 10 >= 2 && min % 10 <= 4) && !(min % 100 >= 12 && min % 100 <= 14)) getMinutes = "минуты";
+        else if (min % 10 == 1 && min % 100 != 11) getMinutes = "минута";
 
         // склоняем часы
-        if ((hour % 10 >= 5 && hour % 10 <= 9) ||  hour % 10 == 0 || hour >= 11 && hour <= 20) getHours = "часов";
-        else if (hour % 10 >= 2 && hour % 10 <= 4) getHours = "часа";
-        else if (hour % 10 == 1) getHours = "час";
+        if ((hour % 10 >= 2 && hour % 10 <= 4) && !(hour % 100 >= 12 && hour % 100 <= 14)) getHours = "часа";
+        else if (hour % 10 == 1 && hour != 11) getHours = "час";
 
         // склоняем дни
-        if ((day % 10 >= 5 && day % 10 <= 6) ||  day % 10 == 0) getDays = "дней";
-        else if (day % 10 >= 2 && day % 10 <= 4) getDays = "дня";
+        if (day % 10 >= 2 && day % 10 <= 4) getDays = "дня";
         else if (day % 10 == 1) getDays = "день";
 
         // склоняем недели
-        if ((week % 10 >= 5 && week % 10 <= 9) ||  week % 10 == 0 || week % 100 >= 11 && week % 100 <= 20) getWeeks = "недель";
-        else if (week % 10 >= 2 && week % 10 <= 4) getWeeks = "недели";
-        else if (week % 10 == 1) getWeeks = "неделя";
+        if ((week % 10 >= 2 && week % 10 <= 4) && !(week % 100 >= 12 && week % 100 <= 14)) getWeeks = "недели";
+        else if (week % 10 == 1 && week != 11) getWeeks = "неделя";
 
         System.out.println(seconds);
         System.out.println(hour + " " + getHours + " " + min + " " + getMinutes + " " + sec + " " + getSeconds);
