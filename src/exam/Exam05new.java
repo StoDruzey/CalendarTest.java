0199575e63b9ed88d4c00113e4f313ace34b3cf6
Serge Labada <sergelabada@gmail.com>
2021-09-08 12:02:47 +0300

    Задание 5 на оценку еще раз переделанное. Теперь мне нравится больше. Спасибо

--- src/exam/Exam05new.java
+++ src/exam/Exam05new.java
@@ -1,8 +1,7 @@
 package exam;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Exam05new {
@@ -13,13 +12,25 @@ public class Exam05new {
         //Примеры уникальности: dog и Dog - одинаковые dog и dogs - разные
         String str = "Listen listen to To the news from today and ? read the text !at the same time. Listen to the news from today without reading the text.";
         String[] strWords = str.toLowerCase().split("\\s*(\\s|,|\"|'|\\?|!|\\.)\\s*");
-        List<String> words = Arrays.asList(strWords);
+        int count = 0; //счетчик уникальных слов
+        Map<String, Long> words = Arrays
+                .stream(strWords)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        System.out.println("Уникальные слова в строке: ");
+        for(Map.Entry<String, Long> w : words.entrySet()) {
+            if (w.getValue() == 1) {
+                count++;
+                System.out.println(w.getKey());
+            }
+        }
+        System.out.println("Количество уникальных слов в строке: " + count);
 
-        System.out.println("Количество уникальных слов в строке: " +
-                words.stream()
-                .filter(s -> Collections.frequency(words, s) == 1)
+        // 2й путь, мне он меньше нравится, потому что я его нагуглил
+        List<String> listWords = Arrays.asList(strWords);
+        System.out.println("2й путь: Количество уникальных слов в строке: " +
+                listWords.stream()
+                .filter(s -> Collections.frequency(listWords, s) == 1)
                 .collect(Collectors.toList())
                 .stream().count());
-
     }
 }
